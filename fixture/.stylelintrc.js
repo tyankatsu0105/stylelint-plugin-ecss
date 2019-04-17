3f6657961c3947ea4fc9bcc090204c555ae6c0d8
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2019-04-18 00:52:39 +0900

    refactor: WIP Add rule declaration-comment-magic-numbers-before

--- fixture/.stylelintrc.js
+++ fixture/.stylelintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: ["../lib/index.js"],
   rules: {
-    "ecss/comment-check-prefix-browser-hacks": true
+    "ecss/comment-check-prefix-browser-hacks": true,
+    "ecss/declaration-comment-magic-numbers-before": true
   }
 };
